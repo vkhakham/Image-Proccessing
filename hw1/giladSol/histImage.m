25a433e3a41c8f16f46647ae30a2d48e655adbba
gilad eini <giladeini@gmail.com>
2016-11-19 22:39:47 +0200

    done func1

--- hw1/giladSol/histImage.m
+++ hw1/giladSol/histImage.m
@@ -1,15 +1,22 @@
 function h = histImage(img)
 
-
+%Input : img - a grayscale image in the range [0..255] 
+%
+%Output: h - image histogram. An array 1x256 such that 
+%        entry i contains the number of pixels in the 
+%        image having grayscale value i-1.
+% 
+%Method: Counts the number of pixels with grayscale 
+%        value i in the range [0 .. 255]. 
 
 %DELETE
-
-img = readImage('barbarasmall.tif');
-
+%img = readImage('barbarasmall.tif');
 %DELETE
 
 h = zeros(1,256);
 for i=0:255
     h(i+1) = length( find(img == i) );
-    disp([ num2str(i) , ':', num2str(h(i+1))])
 end
+
+
+
