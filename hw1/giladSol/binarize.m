85257e726e9bc5822ae0f8a2e0400b96694456e8
gilad eini <giladeini@gmail.com>
2016-11-20 00:16:41 +0200

    q4 - binarize

---
+++ hw1/giladSol/binarize.m
@@ -0,0 +1,28 @@
+function bimg = binarize(img,T) 
+
+% function bimg = binarize(img,T) 
+%  
+% Given an image and a threshold value returns a binary image resulting from global thresholding.
+% 
+% Input:  img - a grayscale image in the range [0..255] 
+%         T -  the threshold value in the range [0 ..255]. 
+% 
+% Output: bimg - a binary image, represented as a grayscale
+%         image with values 0 and 255.
+% 
+% Method: Sets all grayscale values in the image greater than
+%         the threshold T to 255 and all grayscale values
+%         smaller or equal to T to 0. 
+
+%DELETE a
+%1 Line of Matlab code. Do NOT loop over the image – use matrix operations. 
+img = readImage('lighthouse.tif');
+showImage(img);
+T=127;
+%DELETE a
+
+bimg = (img > T) * 255;
+
+%DELETE b 
+showImage(bimg);
+%DELETE b
