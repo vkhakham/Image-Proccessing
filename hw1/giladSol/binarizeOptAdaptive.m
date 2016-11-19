503fa5c5515c221c3d4bd18ab612cde0bbcd31c0
gilad eini <giladeini@gmail.com>
2016-11-20 01:00:05 +0200

    binarizeOptAdaptive  base - need optimalQuantization

---
+++ hw1/giladSol/binarizeOptAdaptive.m
@@ -0,0 +1,24 @@
+function [bimg] = binarizeOptAdaptive(img,winsize)
+
+% function [bimg] = binarizeOptAdaptive(img,winsize)
+%             
+% Given an image, calculates the optimal threshold and returns the optimally thresholded image.
+%   
+% Input:  img - a grayscale image in the range [0..255] . 
+%         winsize – an odd integer > 0. Adaptive thresholding windows are winsize x winsize.
+%                               
+% Output: bimg - a binary image, represented as a grayscale
+%                image with values 0 and 255.
+% 
+% Method: Per image pixel determines the optimal threshold value by searching for optimal threshold
+%         in winsize X winsize window around the pixel. Apply optimalQuantization with N=1 in each window.
+%         If window exceeds image boundaries – use only pixels within the image boundaries.
+
+%DELETE a
+%Note: time consuming when winsize is small. Use large winsize. 
+img = readImage('lighthouse.tif');
+%showImage(img);
+winsize = 1;
+%DELETE a
+      
+bimg = img;
