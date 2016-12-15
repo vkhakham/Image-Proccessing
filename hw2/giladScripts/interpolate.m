50e6bbd592323c4b6c21d87db65ffe32b5d15b5a
gilad eini <giladeini@gmail.com>
2016-12-16 01:56:41 +0200

    int

--- hw2/giladScripts/interpolate.m
+++ hw2/giladScripts/interpolate.m
@@ -20,14 +20,46 @@ function sourceGrayVals = interpolate(img,sourceCoors)
    
     Xs = sourceCoors(1,:);
     Ys = sourceCoors(2,:);
+
+    %out of range fix x
+    indx = find(Xs<1 | Xs>c);%think to do Xs<0 | Xs>r+1
+    Xs(indx) = 1;
+    Ys(indx) = 1;
     
-    newimg = reshape(sourceGrayVals, [r, c]);
-    showImage(img);
-    showImage(newimg);
-    K = img - newimg;
-    clear;
-    close all;
-    clc;
+    %out of range fix y
+    indy = find(Ys<1 | Ys>r);
+    Xs(indy) = 1;
+    Ys(indy) = 1;
+    clear sourceCoors r c;%no need
+    
+    %preparing neighbours vectors
+    %could have spared the next 4 vars but saved time instead of writing
+    %twice each var below (leading to floor of a giant vector calculated twise
+    floorX = floor(Xs);
+    floorY = floor(Ys);
+    ceilX = ceil(Xs);
+    ceilY = ceil(Ys);
+    
+    %NW\NE\SW\SE holds coordinates of that direction
+    %will explain the first, the rest are the same:
+    %NW is top left-> Y grows down so take floor. X grows to the right so
+    %take floor.
+    NW = [floorY(:), floorX(:)];
+    NE = [floorY(:), ceilX(:)]; 
+    SW = [ceilY(:) , floorX(:)];
+    SE = [ceilY(:) , ceilX(:)]; 
+    
+    %all DeltaX is the distance from Xs to his floor.
+    %since Y grows down(ceil Y > Ys) we need the distance from up to down
+    deltaX = Xs - floorX;
+    deltaY = ceilY - Ys;
+    clear Xs Ys floorY floorX ceilY ceilX;%no need
+    
+    %get indexes in img of the colors of direction coordinates
+    %than overwrite the coordinates with colors vector
+    NWind   = sub2ind(size(img), NW(:,1), NW(:,2));
+    NW = img(NWind);
+    
     NEind   = sub2ind(size(img), NE(:,1), NE(:,2));
     NE = img(NEind);
     
@@ -36,6 +68,7 @@ function sourceGrayVals = interpolate(img,sourceCoors)
     
     SEind   = sub2ind(size(img), SE(:,1), SE(:,2));
     SE = img(SEind);
+    clear NWind NEind SWind SEind
     
     %calculate linear inter' to SE and SW ->S (with deltaX)
     %and than do linear inter' to the NE and NW ->N(with deltaX)
@@ -43,6 +76,7 @@ function sourceGrayVals = interpolate(img,sourceCoors)
     S = SE .* deltaX' + SW .* (1 - deltaX') ;
     N = NE .* deltaX' + NW .* (1 - deltaX') ;
     V = N .* deltaY' + S .* (1 - deltaY') ;
+    clear NW NE SW SE img S N deltaX deltaY
     sourceGrayVals = V;
     
     %out of range gets color black. indx and indy were calculated at the begining
