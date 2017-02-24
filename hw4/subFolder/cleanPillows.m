6fc8589f5c6ce83b7f74a6c0afe0273e73a7a4ae
gilad eini <giladeini@gmail.com>
2017-02-24 18:44:26 +0200

    removed pdfs

--- hw4/subFolder/cleanPillows.m
+++
@@ -1,9 +0,0 @@
-%authors: 
-% Vadim Khakham, id 311890156
-% Gilad Eini   , id 034744920
-%
-function cleanIm = cleanPillows (im)
-    im = double(im);
-    median = cleanImageMedian(im, [6,0]);
-    cleanIm = uint8(median);
-end
